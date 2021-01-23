6e23c08dff578dc9635ef1eff4bade9667a05a78
TristanDiabolo <tristan@diabolo-web.com>
2021-01-23 21:45:33 +0100

    [#12/front-end] Display 6 last todo tasks on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * @var TaskRepository
+     */
+    private $taskRepository;
+
+    /**
+     * HomeController constructor.
+     * @param TaskRepository $taskRepository
+     */
+    public function __construct(TaskRepository $taskRepository)
+    {
+        $this->taskRepository = $taskRepository;
+    }
+
+    /**
      * @Route("/", name="homepage")
      */
     public function index(): Response
     {
+        $tasks = $this->taskRepository->findBy([
+            'done' => false,
+            'user' => $this->getUser()->getId(),
+        ], ['createdAt' => 'DESC'], 6);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'tasks' => $tasks
         ]);
     }
 }
