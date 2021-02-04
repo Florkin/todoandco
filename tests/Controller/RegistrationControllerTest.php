85fd7b11ecc2f8e9c00b8e131c1db9b783da4bed
TristanDiabolo <tristan@diabolo-web.com>
2021-02-04 17:21:51 +0100

    [#6/tests] test register

--- tests/Controller/RegistrationControllerTest.php
+++ tests/Controller/RegistrationControllerTest.php
@@ -6,8 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class RegistrationControllerTest extends WebTestCase
 {
-    public function testRegisterUser()
+    public function testRegister()
     {
-
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
+        $form = $crawler->filter('[name="user"]')->form([
+            'user[username]' => 'testUsername',
+            'user[email]' => 'test@test.com',
+            'user[plainPassword][first]' => 'testpassword',
+            'user[plainPassword][second]' => 'testpassword',
+            'user[agreeTerms]' => 1
+        ]);
+        $client->submit($form);
+        $this->assertResponseRedirects('/');
     }
 }
